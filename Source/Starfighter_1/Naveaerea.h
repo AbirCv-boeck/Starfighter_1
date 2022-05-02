27552cdee452fa16708b3c48d4a60be02999522d
AbirCv-boeck <abircvonboeck@gmail.com>
2022-05-02 00:26:54 -0400

    clases

---
+++ Source/Starfighter_1/Naveaerea.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Nave.h"
+#include "Naveaerea.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STARFIGHTER_1_API ANaveaerea : public ANave
+{
+	GENERATED_BODY()
+	
+};
